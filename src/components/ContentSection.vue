9f9a629d045c1c168c0a80d205b1c8061d1e4456
mahsa <mahsa.shatergholi@gmail.com>
2025-04-22 20:25:44 +0330

    Add Boxs

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="flex-1 overflow-y-auto">
-    <div class="bg-gray-200 min-h-[1800px] pt-[23%] text-center">Content with overflow</div>
+    <div class="bg-gray-200 min-h-[1800px] pt-[23%] text-center">
+      Content with overflow
+      <div class="mt-[25px] justify-center items-center flex">
+        <FlexBox />
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+import FlexBox from "./task7/FlexBox.vue";
+</script>
---
+++ src/components/task7/FlexBox.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container p-[20px] bg-black w-full">
+    <div class="content p-[40px]">
+      <div class="box red">1</div>
+      <div class="box blue">2</div>
+      <div class="box green">3</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 300px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+}
+.content {
+  display: flex;
+  gap: 10px;
+  border: #fff solid 2px;
+  border-radius: 20px;
+}
+.box {
+  width: 100%;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+}
+.red {
+  width: 150px;
+  background-color: red;
+  height: auto;
+  min-height: 180px;
+}
+.blue {
+  width: 150px;
+  background-color: blue;
+  height: auto;
+  min-height: 180px;
+}
+.green {
+  width: auto;
+  flex-grow: 1;
+  min-width: 150px;
+  background-color: green;
+  height: auto;
+  min-height: 180px;
+}
+</style>
